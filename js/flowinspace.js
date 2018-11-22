c9caff2cfe170c053115dfadd5e33ed4316301e5
sbiego <forsesim@anche.no>
2018-11-22 07:18:30 +0100

    anim change and min

--- js/flowinspace.js
+++ js/flowinspace.js
@@ -1,14 +1,14 @@
 viewport_h = $(window).height();
 viewport_w = $(window).width();
 
-if(viewport_h > viewport_w) {
+if(viewport_h < viewport_w) {
 	new_height = viewport_h * 1;
 	$('#banner').css("height", new_height);
 
 	// $('#banner').css("transform", "5px");
 } else {
-	new_width = viewport_w * 1;
-	$('#banner').css("width", new_width);
+	// new_width = viewport_w * 1;
+	// $('#banner').css("width", new_width);
 
 }
 
@@ -35,8 +35,8 @@ document.addEventListener('DOMContentLoaded', function () {
 				opacity: 1,
 			}, .3);
 			
-			var starFlows = TweenMax.from($('#stars'), 15, {
-				delay: .3,
+			var starFlows = TweenMax.to($('#stars'), 5, {
+				delay: .5,
 				scale: .9,
 				rotation: 5,
 				ease: Elastic.easeOut,
