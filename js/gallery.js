4c9f850759c3648ae8099021e8c2ba040f1166f1
sbiego <forsesim@anche.no>
2019-05-22 01:55:49 +0200

    galleria/portfolio completa

--- js/gallery.js
+++ js/gallery.js
@@ -1,121 +1,150 @@
 options = {
-        // optionName: 'option value'
-        // for example:
-        index: 0 // start at first slide
-    };
+    // optionName: 'option value'
+    // for example:
+    index: 0 // start at first slide
+};
 
-animazioni = [
-    {	src: 'images/portfolio/animazioni/flyfishLogoTransitionOPT_big.gif',
-        w: 580,
-        h: 380
+animazioni = [{
+    src: 'images/portfolio/animazioni/flyfishLogoTransitionOPT_big.gif',
+    w: 580,
+    h: 380
+}]
+banners = [{
+    src: 'images/portfolio/banner/aperiborda.jpg',
+    w: 740,
+    h: 280
+}, {
+    src: 'images/portfolio/banner/aperijam.jpg',
+    w: 740,
+    h: 280
+}]
+copertine = [{
+    src: 'images/portfolio/copertine/ateovirtuoso.png',
+    w: 440,
+    h: 710
+}, {
+    src: 'images/portfolio/copertine/cronache.jpg',
+    w: 780,
+    h: 380
+}]
+locandine = [{
+        src: 'images/portfolio/locandine/05-19_MostraSenzaParole.png',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/07-04_AperitivoSenzaParole.png',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/1113-08-17_FaroScoglitti.png',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/apesontapes.jpg',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/uochitoki.jpg',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/formediluite_cagliari.jpg',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/endjamsession.jpg',
+        w: 720,
+        h: 500
+    }, {
+        src: 'images/portfolio/locandine/expopolis.jpg',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/dubparty.jpg',
+        w: 500,
+        h: 720
+    }, {
+        src: 'images/portfolio/locandine/rootsmilitant.jpg',
+        w: 500,
+        h: 720
     }
+
 ]
-banners = [
-    {	src: 'images/portfolio/banner/aperiborda.jpg',
-        w: 740,
-        h: 280
-    }
-    , {	src: 'images/portfolio/banner/aperijam.jpg',
-        w: 740,
-        h: 280
-    }
-]
-copertine = [
-    {	src: 'images/portfolio/copertine/ateovirtuoso.png',
-        w: 440,
-        h: 710
-    }
-    , {	src: 'images/portfolio/copertine/cronache.jpg',
-        w: 780,
-        h: 380
-    }
-]
-copertine = [
-    {	src: 'images/portfolio/copertine/ateovirtuoso.png',
-        w: 440,
-        h: 710
-    }
-    , {	src: 'images/portfolio/copertine/cronache.jpg',
-        w: 780,
-        h: 380
-    }
-]
-diffidente = [
-    {	src: 'images/portfolio/diffidente/diffidente_banner.png',
-        w: 680,
-        h: 480
-    }
-    , {	src: 'images/portfolio/diffidente/diffidente1.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/diffidente2.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/diffidente3.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/diffidente4.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/diffidente5.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/diffidente6.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/diffidente7.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/diffidente8.png',
-        w: 680,
-        h: 480
-    }
-    , { src: 'images/portfolio/diffidente/ProtoCollo.png',
-        w: 680,
-        h: 480
-    }
-    , {	src: 'images/portfolio/diffidente/keepcalm1.png',
-        w: 480,
-        h: 680
-    }
-    , { src: 'images/portfolio/diffidente/keepcalm2.png',
-        w: 480,
-        h: 680
-    }
-    , { src: 'images/portfolio/diffidente/keepcalm3.png',
-        w: 480,
-        h: 680
-    }
-    , { src: 'images/portfolio/diffidente/keepcalm4.png',
-        w: 480,
-        h: 680
-    }
-    , { src: 'images/portfolio/diffidente/keepcalm5.png',
-        w: 480,
-        h: 680
-    }
-    , { src: 'images/portfolio/diffidente/keepcalm6.png',
-        w: 480,
-        h: 680
-    }
-    , { src: 'images/portfolio/diffidente/keepcalm7.png',
-        w: 480,
-        h: 680
-    }
-    , { src: 'images/portfolio/diffidente/keepcalm8.png',
-        w: 480,
-        h: 680
-    }
-]
-adesivi = [
-    {	src: 'images/portfolio/adesivi/aqara.png',
+diffidente = [{
+    src: 'images/portfolio/diffidente/diffidente_banner.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente1.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente2.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente3.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente4.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente5.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente6.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente7.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/diffidente8.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/ProtoCollo.png',
+    w: 680,
+    h: 480
+}, {
+    src: 'images/portfolio/diffidente/keepcalm1.png',
+    w: 480,
+    h: 680
+}, {
+    src: 'images/portfolio/diffidente/keepcalm2.png',
+    w: 480,
+    h: 680
+}, {
+    src: 'images/portfolio/diffidente/keepcalm3.png',
+    w: 480,
+    h: 680
+}, {
+    src: 'images/portfolio/diffidente/keepcalm4.png',
+    w: 480,
+    h: 680
+}, {
+    src: 'images/portfolio/diffidente/keepcalm5.png',
+    w: 480,
+    h: 680
+}, {
+    src: 'images/portfolio/diffidente/keepcalm6.png',
+    w: 480,
+    h: 680
+}, {
+    src: 'images/portfolio/diffidente/keepcalm7.png',
+    w: 480,
+    h: 680
+}, {
+    src: 'images/portfolio/diffidente/keepcalm8.png',
+    w: 480,
+    h: 680
+}]
+adesivi = [{
+        src: 'images/portfolio/adesivi/aqara.png',
         w: 300,
         h: 300
     }
@@ -148,94 +177,269 @@ adesivi = [
         src: 'images/portfolio/adesivi/eigenlab_sketch.png',
         w: 430,
         h: 300
-    }
-];
-striker = [
-    {	src: 'images/portfolio/vario/anon_striker.png',
-        w: 300,
-        h: 300
-    }
-    , {
-        src: 'images/portfolio/vario/eigenlab_striker.png',
-        w: 300,
-        h: 300
-    }
-    , {
-        src: 'images/portfolio/vario/grafico_3d.png',
-        w: 300,
-        h: 300
-    }
-    , {
-        src: 'images/portfolio/vario/grafico.png',
-        w: 300,
-        h: 300
-    }
-    , {
-        src: 'images/portfolio/vario/principessa.png',
-        w: 300,
-        h: 300
-    }
-]
+}];
+striker = [{
+    src: 'images/portfolio/vario/anon_striker.png',
+    w: 300,
+    h: 300
+}, {
+    src: 'images/portfolio/vario/eigenlab_striker.png',
+    w: 300,
+    h: 300
+}, {
+    src: 'images/portfolio/vario/grafico_3d.png',
+    w: 300,
+    h: 300
+}, {
+    src: 'images/portfolio/vario/grafico.png',
+    w: 300,
+    h: 300
+}, {
+    src: 'images/portfolio/vario/principessa.png',
+    w: 300,
+    h: 300
+}]
 icone = [{
     src: 'images/portfolio/icone/conversations.jpg',
     w: 400,
     h: 400
+}]
+magliette = [{
+    src: 'images/portfolio/magliette/eigen_fronte.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/eigen_retro.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/hackmeeting_fronte.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/hackmeeting_retro.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/maglia.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/pong.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/suonodita_fronte.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/suonodita_retro.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/tower_fronte.png',
+    h: 847,
+    w: 1000
+}, {
+    src: 'images/portfolio/magliette/tower_retro.png',
+    h: 847,
+    w: 1000
+}]
+
+sfondi = [
+    {src: 'images/portfolio/sfondi/galaxy_blue.jpg',
+        h: 600,
+        w: 840
+    },
+    {src: 'images/portfolio/sfondi/gioiarivoluzione.png',
+        h: 600,
+        w: 860
+    },
+    {src: 'images/portfolio/sfondi/global_warm.png',
+        h: 700,
+        w: 860
+    },
+    {src: 'images/portfolio/sfondi/hack_blue.png',
+        h: 700,
+        w: 980
+    },
+    {src: 'images/portfolio/sfondi/hack_red.png',
+        h: 700,
+        w: 980
+    },
+    {src: 'images/portfolio/sfondi/paper.jpg',
+        h: 640,
+        w: 1000
+    },
+    {src: 'images/portfolio/sfondi/subacquea_wallpaper.jpg',
+        h: 600,
+        w: 1000
+    },
+    {src: 'images/portfolio/sfondi/subacquea_wallpaper2.jpg',
+        h: 600,
+        w: 1000
+    }
+]
+loghi = [{
+    src: 'images/portfolio/loghi/antipong.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/aqara.png',
+    h: 600,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/corpisqualificati.png',
+    h: 210,
+    w: 700
+}, {
+    src: 'images/portfolio/loghi/eigenlab_icon.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/eigenlab.jpg',
+    h: 370,
+    w: 640
+}, {
+    src: 'images/portfolio/loghi/eigennet.jpg',
+    h: 370,
+    w: 860
+}, {
+    src: 'images/portfolio/loghi/eigenwiki.jpg',
+    h: 370,
+    w: 840
+}, {
+    src: 'images/portfolio/loghi/francoproject.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/friconight.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/hackit_bolo.png',
+    h: 450,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/itertour_bicolor_darkbg.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/jastersind.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/kelevra.jpg',
+    h: 200,
+    w: 900
+}, {
+    src: 'images/portfolio/loghi/libersoft.png',
+    h: 200,
+    w: 580
+}, {
+    src: 'images/portfolio/loghi/logo_CorriereDelDiffidato.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/lumia.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/nautilus.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/OrdineSederiLiberi.png',
+    h: 300,
+    w: 550
+}, {
+    src: 'images/portfolio/loghi/populalma_razza.png',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/salvini.jpg',
+    h: 400,
+    w: 500
+}, {
+    src: 'images/portfolio/loghi/subacquea.jpg',
+    h: 400,
+    w: 500
+}, {
+    src: 'images/portfolio/loghi/takeitisee.jpg',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/vamanda_bn.jpg',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/vamanda.jpg',
+    h: 400,
+    w: 400
+}, {
+    src: 'images/portfolio/loghi/vettori.png',
+    h: 260,
+    w: 700
+}, {
+    src: 'images/portfolio/loghi/vision.png',
+    h: 400,
+    w: 400
 }]
 
 
 var pswpElement = document.querySelectorAll('.pswp')[0];
 
 document.getElementById('portfolioStickers').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, adesivi, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, adesivi, options);
     gallery.init();
 }
 
 document.getElementById('portfolioAnimations').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, animazioni, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, animazioni, options);
     gallery.init();
 }
 
 document.getElementById('portfolioStriker').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, striker, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, striker, options);
     gallery.init();
 }
 
 document.getElementById('portfolioBanners').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, banners, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, banners, options);
     gallery.init();
 }
 
 document.getElementById('portfolioCovers').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, copertine, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, copertine, options);
     gallery.init();
 }
 
 document.getElementById('portfolioDiffidente').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, diffidente, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, diffidente, options);
     gallery.init();
 }
 
 document.getElementById('portfolioIcons').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, icone, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, icone, options);
     gallery.init();
 }
 
 document.getElementById('portfolioPosters').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, locandine, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, locandine, options);
     gallery.init();
 }
 
 document.getElementById('portfolioLogos').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, loghi, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, loghi, options);
     gallery.init();
 }
 
 document.getElementById('portfolioShirts').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, magliette, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, magliette, options);
     gallery.init();
 }
 
 document.getElementById('portfolioBackgrounds').onclick = function () {
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, sfondi, options);
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, sfondi, options);
     gallery.init();
 }
